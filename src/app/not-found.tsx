d3c4b4938c49192dffa977324e56dc5f476fb7b4
Hyeongyun <shg7271@gmail.com>
2024-04-02 17:07:10 +0900

    Fix: root 404 page link tag change - production error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,18 +6,21 @@ import Link from "next/link";
 import { revalidatePath } from "next/cache";
 
 export default function NotFound() {
-  revalidatePath("/");
   return (
     <Stack alignItems="center" gap={5} p={5}>
       <Typography variant="h2">404</Typography>
       <Typography variant="h3">Page not found :(</Typography>
       <Box>The requested page could not be found.</Box>
       <Image src={Image404} alt="404 image" />
-      <Link href="/" replace scroll={false}>
+      {/* production error
+      Link 컴포넌트를 사용하는 경우 "/a"와 같이 루트에서 404 페이지 접속 시
+      Back to home 클릭 시 "/"으로 이동하긴 하지만, 페이지가 정상적으로 렌더되지 않고, 404 표현 남아있음
+      */}
+      <a href="/">
         <Button variant="contained">
           <ArrowBack sx={{ mr: 1 }} /> Back to home
         </Button>
-      </Link>
+      </a>
     </Stack>
   );
 }
