490b663dbd475ca4ccb50dff7ff4933d48f2d441
Hyeongyun <shg7271@gmail.com>
2024-04-02 16:42:54 +0900

    Fix: type page error handle, not-found page Link props

--- src/app/type/error.tsx
+++ src/app/type/error.tsx
@@ -1,17 +1,18 @@
+"use client";
 import { Box, Button, Stack, Typography } from "@mui/material";
 import Image from "next/image";
-import Image404 from "./404.webp";
+import Image404 from "../404.webp";
 import { ArrowBack } from "@mui/icons-material";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
     <Stack alignItems="center" gap={5} p={5}>
-      <Typography variant="h2">404</Typography>
-      <Typography variant="h3">Page not found :(</Typography>
+      <Typography variant="h2">Invalid Type</Typography>
+      <Typography variant="h3">Pokemon not found :(</Typography>
       <Box>The requested page could not be found.</Box>
       <Image src={Image404} alt="404 image" />
-      <Link href="/" replace scroll={false}>
+      <Link href="/" scroll={false}>
         <Button variant="contained">
           <ArrowBack sx={{ mr: 1 }} /> Back to home
         </Button>
